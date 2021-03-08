6605f45e53223d93f5b95058e67287a6870f2ed8
Simone Sechi <casasechiulivieri@gmail.com>
2021-03-08 14:17:46 +0100

    seconda versione

--- lib/main.dart
+++ lib/main.dart
@@ -46,36 +46,62 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  bool swipeLeft = false, swipeRight = false;
   List<Widget> funzioneCarina(BuildContext context) {
     CardController controller;
     return <Widget>[
-      new TinderSwapCard(
-        swipeUp: true,
-        swipeDown: true,
-        orientation: AmassOrientation.BOTTOM,
-        totalNum: welcomeImages.length,
-        stackNum: 3,
-        swipeEdge: 4.0,
-        maxWidth: MediaQuery.of(context).size.width * 0.9,
-        maxHeight: MediaQuery.of(context).size.width * 0.9,
-        minWidth: MediaQuery.of(context).size.width * 0.8,
-        minHeight: MediaQuery.of(context).size.width * 0.8,
-        cardBuilder: (context, index) => Card(
-          child: Image.network('${welcomeImages[index]}'),
-          elevation: 20,
-        ),
-        cardController: controller = CardController(),
-        swipeUpdateCallback: (DragUpdateDetails details, Alignment align) {
-          /// Get swiping card's alignment
-          if (align.x < 0) {
-            //Card is LEFT swiping
-          } else if (align.x > 0) {
-            //Card is RIGHT swiping
-          }
-        },
-        swipeCompleteCallback: (CardSwipeOrientation orientation, int index) {
-          print(orientation);
-        },
+      Column(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height * 0.6,
+            child: new TinderSwapCard(
+              swipeUp: false,
+              swipeDown: false,
+              orientation: AmassOrientation.BOTTOM,
+              totalNum: welcomeImages.length,
+              stackNum: 3,
+              swipeEdge: 3.0,
+              maxWidth: MediaQuery.of(context).size.width * 0.9,
+              maxHeight: MediaQuery.of(context).size.width * 0.9,
+              minWidth: MediaQuery.of(context).size.width * 0.8,
+              minHeight: MediaQuery.of(context).size.width * 0.8,
+              cardBuilder: (context, index) => Card(
+                child: Image.network('${welcomeImages[index]}'),
+                elevation: 20,
+              ),
+              cardController: controller = CardController(),
+              swipeUpdateCallback:
+                  (DragUpdateDetails details, Alignment align) {
+                /// Get swiping card's alignment
+                if (align.x < 0) {
+                  setState(() {
+                    swipeLeft = true;
+                    swipeRight = false;
+                  });
+
+                  //Card is LEFT swiping
+                } else if (align.x > 0) {
+                  setState(() {
+                    swipeRight = true;
+                    swipeLeft = false;
+                  });
+                  //Card is RIGHT swiping
+                } else if (align.x == 0) {
+                  setState(() {
+                    swipeRight = false;
+                    swipeLeft = false;
+                  });
+                }
+              },
+              swipeCompleteCallback:
+                  (CardSwipeOrientation orientation, int index) {
+                print(orientation);
+              },
+            ),
+          ),
+          swipeLeft ? Icon(Icons.close) : Container(),
+          swipeRight ? Icon(Icons.check) : Container(),
+        ],
       ),
       Text(
         'Index 2: School',
@@ -117,7 +143,10 @@ class _MyHomePageState extends State<MyHomePage> {
     "https://images.unsplash.com/photo-1612979857678-0ce10e5b3439?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80",
     "https://images.unsplash.com/photo-1613066839141-4489f60e0389?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=634&q=80",
     "https://images.unsplash.com/photo-1613066839141-4489f60e0389?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=634&q=80",
-    "https://images.unsplash.com/photo-1613098169745-015562f6f27a?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=634&q=80"
+    "https://images.unsplash.com/photo-1613098169745-015562f6f27a?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=634&q=80",
+    "https://images.unsplash.com/photo-1612979857678-0ce10e5b3439?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80",
+    "https://images.unsplash.com/photo-1613066839141-4489f60e0389?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=634&q=80",
+    "https://images.unsplash.com/photo-1613066839141-4489f60e0389?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=634&q=80"
   ];
   int _selectedIndex = 0;
   static const TextStyle optionStyle =
