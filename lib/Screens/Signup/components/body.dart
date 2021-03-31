a2f242f07a75811892fa0b964d79ce400e7f2201
Simone Sechi <casasechiulivieri@gmail.com>
2021-03-31 11:22:55 +0200

    aggiunti check password e richiesta finto api
    Former-commit-id: 4ed53e13dc2e20ca64efab9d5f56c5626031fce0
    Former-commit-id: 96b0e97e9dded0332656852f4a3fa99816d8fe75
    Former-commit-id: 2a123c5093f1cf7bf865f64469b1a9c03849a2d6

--- lib/Screens/Signup/components/body.dart
+++ lib/Screens/Signup/components/body.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:finto_spoti/Screens/Login/login_screen.dart';
 import 'package:finto_spoti/Screens/Signup/components/background.dart';
@@ -8,10 +10,14 @@ import 'package:finto_spoti/components/rounded_button.dart';
 import 'package:finto_spoti/components/rounded_input_field.dart';
 import 'package:finto_spoti/components/rounded_password_field.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:http/http.dart' as http;
 
 // ignore: must_be_immutable
 class Body extends StatelessWidget {
   bool isHidden = true;
+  bool isHidden2 = true;
+  String psw_1 = "", psw_2 = "";
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
@@ -34,7 +40,9 @@ class Body extends StatelessWidget {
               onChanged: (value) {},
             ),
             RoundedPasswordField(
-              onChanged: (value) {},
+              onChanged: (value) {
+                psw_1 = value;
+              },
               hidden: isHidden,
               press: () {
                 print(isHidden);
@@ -42,11 +50,31 @@ class Body extends StatelessWidget {
                 (context as Element).markNeedsBuild();
               },
             ),
+            RoundedPasswordField(
+              onChanged: (value) {
+                psw_2 = value;
+              },
+              hidden: isHidden2,
+              press: () {
+                print(isHidden2);
+                isHidden2 = !isHidden2;
+                (context as Element).markNeedsBuild();
+              },
+            ),
             RoundedButton(
               text: "SIGNUP",
               textColor: Colors.white,
               press: () {
-                print("banana");
+                if (psw_1 != psw_2) {
+                  Fluttertoast.showToast(
+                      msg: "Le password non coincidono",
+                      toastLength: Toast.LENGTH_SHORT,
+                      gravity: ToastGravity.CENTER,
+                      timeInSecForIosWeb: 2,
+                      backgroundColor: Colors.red,
+                      textColor: Colors.white,
+                      fontSize: 24.0);
+                } else {}
               },
             ),
             SizedBox(height: size.height * 0.03),
@@ -84,6 +112,18 @@ class Body extends StatelessWidget {
           ],
         ),
       ),
+    );
+  }
+
+  Future<http.Response> registerAction() {
+    return http.post(
+      Uri.https('jsonplaceholder.typicode.com', 'albums'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{
+        'title': "ciao",
+      }),
     );
   }
 }
